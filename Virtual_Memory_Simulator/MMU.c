d30d56cb212e8fcb98f69940b5e490ac31c30811
Alessio Mason <mason.1934113@studenti.uniroma1.it>
2024-08-27 21:51:30 +0200

    page_table.h and .c added

--- Virtual_Memory_Simulator/MMU.c
+++ Virtual_Memory_Simulator/MMU.c
@@ -1,21 +1,9 @@
+#pragma once
+
 #include "MMU.h"
-#include "assert.h"
-
-void PageTable_init(PageTable* pt, uint32_t* pt_len, char* phys_mem){
-    assert(pt && "Page table poiter must not be NULL");
-    assert(phys_mem && "Physical memory poiter must not be NULL");
-    pt = (PageTable*) phys_mem;
-
-    for(uint32_t i = 0; i < PAGES; ++i){
-        pt->table[i].frame_index = -1;
-        pt->table[i].unswappable = 0;
-        pt->table[i].valid = 0;
-        pt->table[i].read = 0;
-        pt->table[i].write = 0;
-    }
-
-    *pt_len = sizeof(PageTable);    
-}
+#include "swap.h"
+#include <stdio.h>
+#include <assert.h>
 
 void MMU_init(MMU* mmu, char* phys_mem){
     assert(mmu && "MMU poiter must not be NULL");
@@ -38,21 +26,25 @@ void MMU_init(MMU* mmu, char* phys_mem){
     mmu->free_list.list = (int*)(phys_mem + mmu->pt_len);
 
     ArrayList_init(&(mmu->free_list), pages_for_data_structures, FRAMES, FRAMES);
+
+    Swap_init(SWAP_FILE);
 }
 
-uint32_t getPhysicalAddress(PageTable* pt, uint32_t virt_addr){
-    uint32_t offset_mask = (1 << PAGE_OFFSET_SIZE) - 1;
-    uint32_t page_offset = virt_addr & offset_mask;
-    uint32_t page_index = (virt_addr >> PAGE_OFFSET_SIZE);
-
-    uint32_t frame_index = pt->table[page_index].frame_index;
-    uint32_t phys_addr = (frame_index << PAGE_OFFSET_SIZE) | page_offset;
-
-    return phys_addr;
-}
-
-void MMU_writeByte(MMU* mmu, uint32_t pos, char c){
+void MMU_writeByte(MMU* mmu, uint32_t virt_addr, char c){
     
 }
-char* MMU_readByte(MMU* mmu, uint32_t pos);
-void MMU_exception(MMU* mmu, uint32_t pos);
+
+char* MMU_readByte(MMU* mmu, uint32_t virt_addr){
+    AddressingResult res = AddressIsValid(mmu->pt, virt_addr);
+    if(res == Valid){
+        uint32_t page_index = getPageIndex(virt_addr);
+        mmu->pt->table[page_index].read = 1;
+        uint32_t phys_addr = getPhysicalAddress(mmu->pt, virt_addr);
+        return &(mmu->mem_ptr[phys_addr]);
+    }
+    else if(res == Invalid){
+        printf("Address not valid\n");
+        return NULL;
+    }
+}
+void MMU_exception(MMU* mmu, uint32_t virt_addr);
--- Virtual_Memory_Simulator/MMU.h
+++ Virtual_Memory_Simulator/MMU.h
@@ -1,19 +1,7 @@
 #pragma once
 
-#include "VMSS_constants.h"
+#include "page_table.h"
 #include "array_list.h"
-
-typedef struct Page{
-    uint32_t frame_index: FRAME_INDEX_SIZE;
-    uint8_t unswappable: 1;
-    uint8_t valid: 1;
-    uint8_t read: 1;
-    uint8_t write: 1;
-} Page;
-
-typedef struct PageTable{
-    Page table[PAGES];
-} PageTable;
 
 typedef struct MMU{
     char* mem_ptr;
@@ -23,12 +11,10 @@ typedef struct MMU{
     ArrayList free_list;
 } MMU;
 
-void PageTable_init(PageTable* pt, uint32_t* pt_len, char* phys_mem);
 void MMU_init(MMU* mmu, char* phys_mem);
 
-uint32_t getPhysicalAddress(PageTable* pt, uint32_t virt_addr);
 int isValidAddress();
 
-void MMU_writeByte(MMU* mmu, uint32_t pos, char c);
-char* MMU_readByte(MMU* mmu, uint32_t pos);
-void MMU_exception(MMU* mmu, uint32_t pos);
+void MMU_writeByte(MMU* mmu, uint32_t virt_addr, char c);
+char* MMU_readByte(MMU* mmu, uint32_t virt_addr);
+void MMU_exception(MMU* mmu, uint32_t virt_addr);
